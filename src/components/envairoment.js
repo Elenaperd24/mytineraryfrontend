3c64427963780b746d9cd75002deb658c8ada025
Elenaperd24 <elena24.perdomo@gmail.com>
2023-03-27 18:38:34 +1100

    .

--- src/components/envairoment.js
+++ src/components/envairoment.js
@@ -1,2 +1,4 @@
 // export const _url = "http://13.238.117.124:5000/" 
- export const _url =  "https:/mytinerary.fly.dev/"  
+// export const _url =  "https:/mytinerary.fly.dev/"  
+
+ export const _url = "https:/severmytinerary.fly.dev/" 
