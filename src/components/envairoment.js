8d338a4240aa3374e635931d5051c99994e86a8b
Elenaperd24 <elena24.perdomo@gmail.com>
2023-10-15 22:33:03 -0300

    url api

--- src/components/envairoment.js
+++ src/components/envairoment.js
@@ -1,6 +1,6 @@
 // export const _url = "http://13.238.117.124:5000/" 
 // export const _url =  "https:/mytinerary.fly.dev/"  
 
-//  export const _url = "https://severmytinerary.fly.dev/" 
+ export const _url = "https://severmytinerary.fly.dev/" 
 
- export const _url ="http://localhost:5000/"
+//  export const _url ="http://localhost:5000/"
